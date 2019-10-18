388aacdfe94d23c586a4454e625e282181cd6f01
zkhin <arandomname@gmail.com>
2019-10-18 12:26:36 -0400

    react zeit deployment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Zayar
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
